9889b4dbffbc98688b917c04f77bc42ce90352bd
Elias Atalay <selasie7@yahoo.com>
2022-08-21 21:03:53 +0200

    loggedInMessage

--- client1/src/Loginpage.js
+++ client1/src/Loginpage.js
@@ -2,17 +2,52 @@ import "./style.css";
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 
+
+/**useEffect(() => {
+    Axios.get("http://localhost:3001/getUsers").then((response) => {
+      setListOfUsers(response.data);
+    });
+  }, []);
+ */
+
 function Loginpage() {
-  function loginUser(){
-    alert("logged in")
+
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [username, setUsername] = useState();
+  const [passowrd, setPassword] = useState();
+  const [userlogged, setUserlogged] = useState(false);
+
+  function loginUser() {
+    console.log("listOfUsers", listOfUsers)
+    console.log("UserName", username)
+    console.log("Password", passowrd)
+    //alert("logged in")
+    Axios.get("http://localhost:3001/getUsers").then((response) => {
+      setListOfUsers(response.data);
+      listOfUsers.forEach((user) => {
+        if ((user.name === username) && (user.username === passowrd)) {
+          setUserlogged(true)
+          console.log("userlogged")
+        }
+      })
+
+    });
   }
   return (
     <div>
-      <div className="creatUser">
-        <input type="text" placeholder="UserName..." />
-        <input type="text" placeholder="Password..." />
-        <button onClick={loginUser}> log In </button>
-      </div>
+      {!userlogged ?
+        <div className="creatUser">
+          <input type="text" placeholder="UserName..." onChange={(e) => { setUsername(e.target.value) }} />
+          <input type="text" placeholder="Password..." onChange={(e) => { setPassword(e.target.value) }} />
+          <button onClick={loginUser}> log In </button>
+        </div> :
+        <div>
+          user logged <br></br>
+          Name: {username}
+          <br></br>
+          Password: {passowrd}
+        </div>
+      }
     </div>
   );
 }
